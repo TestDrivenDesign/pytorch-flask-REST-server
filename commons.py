ccddbd5759fa34b947b9fce3b6768fa87f4d8d13
mick nice <mikemccullagh@outlook.com>
2023-04-10 18:11:56 +0100

    fix

--- commons.py
+++ commons.py
@@ -16,25 +16,42 @@ transform = transforms.Compose([
 # defining nn architecture andd forward pass method
 def get_net():
     class Net(nn.Module):
-        def __init__(self):
-            super(Net, self).__init__()
-            self.conv1 = nn.Conv2d(3, 32, 3)
-            self.pool = nn.MaxPool2d(2, 2)
-            self.conv2 = nn.Conv2d(32, 128, 3)
-            self.conv3 = nn.Conv2d(128, 128, 3)
-            self.fc1 = nn.Linear(int(8192/16), 1024)
-            self.fc2 = nn.Linear(1024, 256)
-            self.fc3 = nn.Linear(256, 7)
-    
-        def forward(self, x):
-            x = self.pool(F.relu(self.conv1(x)))
-            x = self.pool(F.relu(self.conv2(x)))
-            x = self.pool(F.relu(self.conv3(x)))   
-            x = x.view(-1, int(8192/16))
-            x = F.relu(self.fc1(x))
-            x = F.relu(self.fc2(x))
-            x = self.fc3(x)
-            return x
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, 3)
+        self.norm1 = nn.LayerNorm([32, 15, 15])
+        self.norm2 = nn.LayerNorm([128, 6, 6])
+        self.norm3 = nn.LayerNorm([128, 2, 2])
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(32, 128, 3)
+        self.conv3 = nn.Conv2d(128, 128, 3)
+        self.fc1 = nn.Linear(int(8192/16), 1024)
+        self.fc2 = nn.Linear(1024, 256)
+        self.fc3 = nn.Linear(256, 7)
+        self.drop1 = nn.Dropout(0.25)
+        self.drop2 = nn.Dropout(0.15)
+        self.use_norm = True
+        self.use_dropout = True
+ 
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        if self.use_norm:
+            x = self.norm1(x)
+        x = self.pool(F.relu(self.conv2(x)))
+        if self.use_norm:
+            x = self.norm2(x)
+        x = self.pool(F.relu(self.conv3(x)))
+        if self.use_norm:
+            x = self.norm3(x)
+        x = x.view(-1, int(8192/16))
+        x = F.relu(self.fc1(x))
+        if self.use_dropout:
+            x = self.drop1(x)
+        x = F.relu(self.fc2(x))
+        if self.use_dropout:
+            x = self.drop2(x)
+        x = self.fc3(x)
+        return x
 
     PATH = './test_net.pth'
     net = Net().to('cpu')
